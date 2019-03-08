2794036b056328135debe4a4073b1b20ab87f40f
albert19920701 <13057508157@163.com>
2019-03-08 15:41:41 +0800

    'albert'

---
+++ src/paybase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 13057
+ * Date: 2019/3/8
+ * Time: 15:16
+ */
+namespace wqpay;
+class paybase
+{
+    public function __construct()
+    {
+        echo 'just a test';
+    }
+}
--- test.php
+++ test.php
@@ -5,11 +5,11 @@
  * Date: 2019/3/6
  * Time: 11:37
  */
-namespace Repository\wqpay;
+namespace wqpay;
 
 class test
 {
-    public  function index()
+    public static  function index()
     {
         echo 'weique payments test';
     }
